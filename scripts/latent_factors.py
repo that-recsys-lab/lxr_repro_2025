46dabb973f14fdff74ca6115ecb66694f616f417
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-05-09 20:26:33 -0600

    Reorder functions and make all private except predict

--- scripts/latent_factors.py
+++ scripts/latent_factors.py
@@ -6,6 +6,7 @@ from help_functions import get_index_in_the_list
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 export_dir = os.getcwd()
+SEED = 42
 
 
 class LatentFactorExplainer:
@@ -21,7 +22,38 @@ class LatentFactorExplainer:
         self.items_array=kw_dict['items_array']
         self.device=kw_dict['device']
 
-    def find_mask(self, user_tensor, targ_id):
+    def predict(self):
+        torch.manual_seed(SEED)
+        np.random.seed(SEED)
+
+        MPRR_Row, MPRR_Percent=[],[]
+        num_of_rand_users = self.test_array.shape[0]   # number of users for evaluations 
+        random_rows = np.random.choice(self.test_array.shape[0], num_of_rand_users, replace=False)
+        random_sampled_array = self.test_array[random_rows]
+
+        for j in range(random_sampled_array.shape[0]):
+            user_id = random_sampled_array[j][-1]
+            user_tensor = torch.Tensor(random_sampled_array[j][:-1]).to(self.device)
+            targ = np.random.choice(self.targ_test[user_id])
+            targ_indx=list(self.targ_test[user_id]).index(targ)
+            p = self._calculate_explanation (user_tensor, targ, targ_indx, k=10)
+
+            if p is not None:
+                MPRR_Row.append(p)
+                MPRR_Percent.append(p/int(torch.sum(user_tensor)))
+
+        print(f'MPNR Row for latent factors similarity for {self.data_name} and {self.recommender_name}:', np.mean(MPRR_Row))
+        print(f'Coverage for latent factors similarity for {self.data_name} and {self.recommender_name}:', len(MPRR_Row)*100/num_of_rand_users)
+
+    def _calculate_explanation (self, user_tensor, targ_id, targ_idx, k):
+        # Find mask
+        m = self._find_mask(user_tensor, targ_id)
+        #### total items to be removed from the users profile 
+        total_items=self._process_sim_items(m, targ_id, targ_idx, user_tensor, k=10)
+    
+        return total_items
+    
+    def _find_mask(self, user_tensor, targ_id):
         """ computing mask for each user """
 
         targ_embedd=self.item_embeddings[:,targ_id]
@@ -31,6 +63,21 @@ class LatentFactorExplainer:
             score=self._cosine_similarity_manual(targ_embedd,user_emebdd)
             item_sim_dict[itm]=score
         return item_sim_dict
+    
+    def _process_sim_items(self, mask, targ_id, targ_idx, user_tensor, k):
+        sorted_m = list(sorted(mask.items(), key=lambda item: item[1], reverse=True))
+        total_items = 0 ## total perturbation
+
+        for i in sorted_m: #i is never used -- should this be in a loop?
+            total_items += 1
+            p = self._mask_items(user_tensor, sorted_m, total_items)
+            kw_dict=self.kw_dict ##index of target item 
+            i1_rank = get_index_in_the_list(p, user_tensor, targ_id,self.recommender, **kw_dict) + 1
+            
+            if (i1_rank > targ_idx + k):
+                return total_items
+
+        return None
     
     def _cosine_similarity_manual(self,vec1, vec2):
         """Compute cosine similarity between two vectors manually."""
@@ -46,7 +93,7 @@ class LatentFactorExplainer:
         similarity = dot_product / (norm_vec1 * norm_vec2)
         return similarity
     
-    def mask_items(self,user_tensor, m1, p):
+    def _mask_items(self,user_tensor, m1, p):
         # Helper function to mask items
         mask = torch.zeros_like(user_tensor, dtype=torch.float32, device=self.device)
         indices = [int(item[0]) for item in m1[:p]]
@@ -55,73 +102,4 @@ class LatentFactorExplainer:
         return user_tensor - mask
     
 
-
-    def process_sim_items(self, mask, targ_id, targ_idx, user_tensor, k):
-        
-        sorted_m = list(sorted(mask.items(), key=lambda item: item[1], reverse=True))
-
-        total_items = 0 ## total perturbation
-
-        for i in sorted_m:
-
-            total_items += 1
-            p = self.mask_items(user_tensor, sorted_m, total_items)
-            
-            ##index of target item 
-            kw_dict=self.kw_dict
-            i1_rank = get_index_in_the_list(p, user_tensor, targ_id,self.recommender, **kw_dict) + 1
-            
-            if (i1_rank > targ_idx + k):
-                #return total_items, i, Indic_score
-                return total_items
-
-
     
-        return None
-    
-
-    
-    def Calculate_Explanation (self, user_tensor, targ_id, targ_idx, k):
-
-        # Find mask
-        m = self.find_mask(
-            user_tensor, targ_id)
-        #### total items to be removed from the users profile 
-        total_items=self.process_sim_items(
-            m, targ_id, targ_idx, user_tensor, k=10)
-    
-
-        return total_items
-    
-
-
-
-
-    def predict(self):
-
-        torch.manual_seed(42)
-        np.random.seed(42)
-
-        MPRR_Row, MPRR_Percent=[],[]
-        num_of_rand_users = self.test_array.shape[0]   # number of users for evaluations 
-        random_rows = np.random.choice(self.test_array.shape[0], num_of_rand_users, replace=False)
-        random_sampled_array = self.test_array[random_rows]
-
-
-        for j in range(random_sampled_array.shape[0]):
-            
-            user_id = random_sampled_array[j][-1]
-            user_tensor = torch.Tensor(random_sampled_array[j][:-1]).to(self.device)
-            targ = np.random.choice(self.targ_test[user_id])
-            targ_indx=list(self.targ_test[user_id]).index(targ)
-            targ_vector = self.items_array[targ]
-            targ_tensor = torch.Tensor(targ_vector).to(self.device)
-
-            p = self.Calculate_Explanation (user_tensor, targ , targ_indx, k=10)
-
-            if p is not None:
-                MPRR_Row.append(p)
-                MPRR_Percent.append(p/int(torch.sum(user_tensor)))
-
-        print(f'MPNR Row for latent factors similarity for {self.data_name} and {self.recommender_name}:', np.mean(MPRR_Row))
-        print(f'Coverage for latent factors similarity for {self.data_name} and {self.recommender_name}:', len(MPRR_Row)*100/num_of_rand_users)
