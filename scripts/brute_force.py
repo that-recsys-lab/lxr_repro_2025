41511bca26968f4b4d94807af94454dfa0395ae1
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-05-09 20:16:08 -0600

    Added constants to replace magic numbers

--- scripts/brute_force.py
+++ scripts/brute_force.py
@@ -5,12 +5,15 @@ import pandas as pd
 import pickle
 import torch
 
-from help_functions import get_index_in_the_list #not sure where help_functions is coming from -- is this supposed to ben internal module?
+from help_functions import get_index_in_the_list #not sure where help_functions is coming from -- is this supposed to be an internal module?
 
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 export_dir = os.getcwd()
 
+THRESHOLD_VAL = 5 #verify good constant name
+GLOBAL_INDEX = 49 #verify good constant name
+INDEX_THRESHOLD = 10 #verify good constant name
 
 class BruteForceEvaluator:
     def __init__(self, data_name, recommender_name, recommender, kw_dict):
@@ -20,10 +23,6 @@ class BruteForceEvaluator:
         self.kw_dict = kw_dict
         self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
         self._load_lxr_outputs()
-
-    def _load_pickle(self, filename):
-        with open(filename, 'rb') as f:
-            return pickle.load(f)
 
     def _load_lxr_outputs(self):
         d = self.data_name
@@ -36,29 +35,30 @@ class BruteForceEvaluator:
         self.Targ_test = self._load_pickle(f'Targ_Test_LXR_{d}_{r}.pkl')
         self.Targ_INDX = self._load_pickle(f'Targ_INDX_LXR_{d}_{r}.pkl')
 
-
-
+    def _load_pickle(self, filename):
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
         
     def evaluate(self):
-        indices = [i for i, val in enumerate(self.MPRR_R[49]) if val < 5]
+        indices = [i for i, val in enumerate(self.MPRR_R[GLOBAL_INDEX]) if val < THRESHOLD_VAL]
         MPNR_lxr = []
         MPNR_bf = []
 
         for j in indices:
-            user_tensor = self.User_Tensor[49][j].to(self.device)
-            pert = self.Mask[49][j].to(self.device)
+            user_tensor = self.User_Tensor[GLOBAL_INDEX][j].to(self.device)
+            pert = self.Mask[GLOBAL_INDEX][j].to(self.device)
             vec = torch.nonzero(pert).view(-1).tolist()
             all_combinations = [list(c) for r in range(1, len(vec) + 1) for c in combinations(vec, r)]
-            MPNR_lxr.append(self.MPRR_R[49][j])
+            MPNR_lxr.append(self.MPRR_R[GLOBAL_INDEX][j])
 
             for i in all_combinations:
                 mask = torch.zeros_like(user_tensor, device=self.device)
                 mask[i] = 1
                 p = user_tensor - mask
-                indx = get_index_in_the_list(p, user_tensor, self.Targ_test[49][j], self.recommender, **self.kw_dict) + 1
+                indx = get_index_in_the_list(p, user_tensor, self.Targ_test[GLOBAL_INDEX][j], self.recommender, **self.kw_dict) + 1
 
-                if indx > 10 + self.Targ_INDX[49][j]:
-                    MPNR_lxr.append(self.MPRR_R[49][j])
+                if indx > INDEX_THRESHOLD + self.Targ_INDX[GLOBAL_INDEX][j]:
+                    MPNR_lxr.append(self.MPRR_R[GLOBAL_INDEX][j])
                     MPNR_bf.append(len(i))
                     break
 
