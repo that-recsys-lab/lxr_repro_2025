0e92c3e7ddfb810ecea30c9dde529fbb01819cbe
meysamvarasteh92 <meysamvarasteh@colorado.edu>
2025-05-09 22:08:04 -0600

    LXR files

---
+++ scripts/Evaluation.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from help_functions import  get_index_in_the_list
+
+
+
+
+class Evaluation(nn.Module):
+
+    def __init__(self, explainer, recommender, kw_dict, k):
+        super(Evaluation, self).__init__()
+        self.explainer = explainer
+        self.recommender = recommender
+        self.kw_dict = kw_dict
+        self.k = k
+
+
+    def forward(self, user_tensor, i1, i1_index, i1_tensor):
+
+        ''' i1 is the target item id and i1-indx is the index (rank) of target item '''
+
+        user_hist_size = int(torch.sum(user_tensor))
+        m1 = self.find_LXR_mask(user_tensor, i1_tensor)
+        return self.process_sim_items(
+            m1, i1, i1_index, user_tensor, user_hist_size)
+    
+
+    def find_LXR_mask(self, user_tensor, i1_tensor):
+        m1 = self.explainer(user_tensor, i1_tensor)
+        x_m1 = user_tensor * m1
+        return {i: x_m1[i].item() for i in range(len(x_m1))}
+
+
+    def mask_items(self, user_tensor, sim_items, total_items):
+        mask = torch.zeros_like(user_tensor, dtype=torch.float32, device=user_tensor.device)
+        indices = [item[0] for item in sim_items[:total_items]]
+        mask[indices] = 1
+        return user_tensor - mask
+
+
+    def process_sim_items(self, m1, i1, i1_index, user_tensor, user_hist_size):
+        sorted_m1 = list(sorted(m1.items(), key=lambda item: item[1], reverse=True))[:user_hist_size]
+        total_items = 0
+        for _ in sorted_m1:
+            total_items += 1
+            p = self.mask_items(user_tensor, sorted_m1, total_items)
+            i1_rank = get_index_in_the_list(p, user_tensor, i1, self.recommender, **self.kw_dict) + 1
+            if i1_rank > i1_index + self.k:
+                return total_items
+        return None
---
+++ scripts/Explainer.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+import numpy as np
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+export_dir = os.getcwd()
+from pathlib import Path
+import torch
+import torch.nn as nn
+
+
+
+
+
+class Explainer(nn.Module):
+    def __init__(self, user_size, item_size, hidden_size):
+        super(Explainer, self).__init__()
+        self.users_fc = nn.Linear(in_features = user_size, out_features=hidden_size)
+        self.items_fc = nn.Linear(in_features = item_size, out_features=hidden_size)
+        self.bottleneck = nn.Sequential(
+            nn.Tanh(),
+            nn.Linear(in_features = hidden_size*2, out_features=hidden_size),
+            nn.Tanh(),
+            nn.Linear(in_features = hidden_size, out_features=user_size),
+            nn.Sigmoid()
+        )
+        
+        
+    def forward(self, user_tensor, item_tensor):
+        user_output = self.users_fc(user_tensor.float())
+        item_output = self.items_fc(item_tensor.float())
+        combined_output = torch.cat((user_output, item_output), dim=-1)
+        expl_scores = self.bottleneck(combined_output)
+        
+        return expl_scores
