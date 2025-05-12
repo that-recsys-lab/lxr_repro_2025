2c1137625cdd40f8cabd522481c26430ae5df260
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-05-11 21:30:21 -0600

    Update load_data.py
    Added comments, reordered imports, deleted unnecessary imports, created constant of size 10

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,17 +1,15 @@
-import os
-from pathlib import Path
 import numpy as np
+import os
 import pandas as pd
-import torch
 import pickle
+import torch
+from pathlib import Path
+
 from help_functions import Help_Functions
 
+TARGET_LIST_SIZE = 10
 
-
-
-
-def load_data(data_name, recommender_name, kw_Dict):
-    """
+'''
     Load and preprocess training and test data for a given dataset.
 
     Args:
@@ -20,8 +18,8 @@ def load_data(data_name, recommender_name, kw_Dict):
 
     Returns:
         Tuple[np.ndarray, np.ndarray]: Processed train and test data arrays.
-    """
-
+'''
+def load_data(data_name, recommender_name, kw_Dict): #I understand recommender_name is being ept for compatibility BUT it is easier to read if unused parameters are deleted...
     DP_DIR = Path("processed_data", data_name) 
     export_dir = Path(os.getcwd())
     files_path = Path(export_dir, DP_DIR)
@@ -29,7 +27,6 @@ def load_data(data_name, recommender_name, kw_Dict):
 
     if num_items is None:
         raise ValueError(f"Unknown dataset: {data_name}")
-
 
     # Load data
     train_data = pd.read_csv(Path(files_path,f'train_data_{data_name}.csv'), index_col=0)
@@ -47,9 +44,6 @@ def load_data(data_name, recommender_name, kw_Dict):
     
     all_items_tensor = torch.Tensor(items_array).to(kw_Dict['device'])
     
-
-
-
     # Vectorized in-place removal: set target item column to 0 in static_test_data
     target_indices = static_test_data.iloc[:, -2].astype(int).values
     rows = np.arange(static_test_data.shape[0])
@@ -59,8 +53,6 @@ def load_data(data_name, recommender_name, kw_Dict):
     for key, value in pop_dict.items():
         pop_array[key] = value
 
-    #test_array = static_test_data.iloc[:, :-2].to_numpy()
-
     return {'train_array': train_array,
          'test_array': test_array,
          'train_data': train_data,
@@ -69,43 +61,45 @@ def load_data(data_name, recommender_name, kw_Dict):
          'items_array': items_array,
          'static_test_data': static_test_data,
          'all_items_tensor': all_items_tensor
-
     }
 
+'''
+    Load / create top recommended items dict
+    data_name:
+    recommender_name:
+    recommender:
+    kw_dict:
+    returns:
 
-
-
-## Load / create top recommended items dict
-
-def targ_item( data_name, recommender_name, recommender, kw_dict):
-    
+    NOTE: function appears to be unused; delete if unused
+    Did leave some comments but didn't redo this function as much as I would've,
+    because it might just get deleted
+'''
+def targ_item(data_name, recommender_name, recommender, kw_dict):
     base_path = Path("processed_data") / data_name
     full_path = Path(os.getcwd()) / base_path
-    device=kw_dict['device']
-    dic=load_data(data_name, recommender_name, kw_dict)
-    hf=Help_Functions(recommender, data_name, recommender_name, kw_dict)
+    device = kw_dict['device']
+    dic = load_data(data_name, recommender_name, kw_dict)
+    hf = Help_Functions(recommender, data_name, recommender_name, kw_dict)
 
-    train_array=dic['train_array']
-    test_array=dic['test_array']
-
+    train_array = dic['train_array']
+    test_array = dic['test_array']
 
     ## target item for training and testing
     targ_train, targ_test = {}, {}
     
-    for i in range(train_array.shape[0]):
+    for i in range(train_array.shape[0]): #suggest making this chunk (for loop + writing to file) a function as it's the same for train/test
         user_index = train_array[i][-1]
         user_tensor = torch.Tensor(train_array[i][:-1]).to(device)
-        recomm_list=hf.get_user_recommended_item(user_tensor)
+        recomm_list = hf.get_user_recommended_item(user_tensor)
         ## Sampling for the target item 
-        targ_train[user_index] = np.array(recomm_list[0:10].cpu())
-    for i in range(test_array.shape[0]):
+        targ_train[user_index] = np.array(recomm_list[0:TARGET_LIST_SIZE].cpu())
+    for i in range(test_array.shape[0]): #instead do for item in test_array?
         user_index = test_array[i][-1]
         user_tensor = torch.Tensor(test_array[i][:-1]).to(device)
-        recomm_list=hf.get_user_recommended_item(user_tensor)
+        recomm_list = hf.get_user_recommended_item(user_tensor)
         ## Sampling for the target item 
-        targ_test[user_index] = np.array(recomm_list[0:10].cpu())
-
-    
+        targ_test[user_index] = np.array(recomm_list[0:TARGET_LIST_SIZE].cpu())
 
     with open(Path(full_path,f'targ_train_{data_name}_{recommender_name}.pkl'), 'wb') as f:
         pickle.dump(targ_train, f)
@@ -113,6 +107,4 @@ def targ_item( data_name, recommender_name, recommender, kw_dict):
     with open(Path(full_path,f'targ_test_{data_name}_{recommender_name}.pkl'), 'wb') as f:
         pickle.dump(targ_test, f)
     
-
-   
     return targ_train, targ_test
