b936a0eacf37b5c7e3fc21e3ec4a2042fa4d9ebd
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-05-11 18:37:59 -0600

    Update Explainer.py
    Added comments, rearranged imports, deleted unnecessary imports

--- scripts/Explainer.py
+++ scripts/Explainer.py
@@ -1,31 +1,33 @@
-
-import pandas as pd
-import numpy as np
 import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-export_dir = os.getcwd()
-from pathlib import Path
 import torch
 import torch.nn as nn
 
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+export_dir = os.getcwd()
 
 
-
-
+'''
+    CLASS DESCRIPTION
+'''
 class Explainer(nn.Module):
     def __init__(self, user_size, item_size, hidden_size):
         super(Explainer, self).__init__()
-        self.users_fc = nn.Linear(in_features = user_size, out_features=hidden_size)
-        self.items_fc = nn.Linear(in_features = item_size, out_features=hidden_size)
+        self.users_fc = nn.Linear(in_features = user_size, out_features = hidden_size)
+        self.items_fc = nn.Linear(in_features = item_size, out_features = hidden_size)
         self.bottleneck = nn.Sequential(
             nn.Tanh(),
-            nn.Linear(in_features = hidden_size*2, out_features=hidden_size),
+            nn.Linear(in_features = hidden_size * 2, out_features = hidden_size),
             nn.Tanh(),
-            nn.Linear(in_features = hidden_size, out_features=user_size),
+            nn.Linear(in_features = hidden_size, out_features = user_size),
             nn.Sigmoid()
         )
         
-        
+    '''
+        DESCRIPTION OF METHOD
+        user_tensor: tensor representing the user
+        item_tensor: 
+        returns: ???
+    '''
     def forward(self, user_tensor, item_tensor):
         user_output = self.users_fc(user_tensor.float())
         item_output = self.items_fc(item_tensor.float())
