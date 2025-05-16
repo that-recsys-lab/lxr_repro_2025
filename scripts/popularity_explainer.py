3fdfadf807ed0798200004d2d30e152ebf6b525c
meysamvarasteh92 <meysamvarasteh@colorado.edu>
2025-05-16 14:50:36 -0600

    pop

--- scripts/popularity_explainer.py
+++ scripts/popularity_explainer.py
@@ -9,14 +9,14 @@ from scripts.help_functions import Help_Functions
 from scripts.recommender.recommenders_architecture import MLP, VAE
 from scripts.Config_Kw_Dict import get_kw_dict
 from scripts.load_data import load_data, targ_item
-
+import pickle
 
 
 
 
 
 class PopularityExplainer:
-    def __init__ (self,recommender_name, data_name ):
+    def __init__ (self,recommender_name, data_name,task ):
             self.data_name=data_name
             self.recommender_name=recommender_name
             self.kw_dict=get_kw_dict()
@@ -26,7 +26,7 @@ class PopularityExplainer:
             #self.items_array=self.kw_dict['items_array']
             self.recommender=self.load_recommender(recommender_name)
             self.hf=Help_Functions(self.recommender, self.data_name, self.recommender_name,self.kw_dict)
-
+            self.task=task
 
 
 
@@ -40,6 +40,7 @@ class PopularityExplainer:
             recommender = MLP(self.data_name, **kw_dict)
         elif recommender_name=='VAE':
             recommender = VAE(self.data_name, **kw_dict)
+
         recommender_checkpoint = torch.load(Path(kw_dict['checkpoints_path'], kw_dict['recommender_path'][(self.data_name, recommender_name)] ), map_location=kw_dict['device'])
         recommender.load_state_dict(recommender_checkpoint)
         recommender.eval()
@@ -103,7 +104,17 @@ class PopularityExplainer:
         total_items=self.process_sim_items(pop_sim_items, targ_id,targ_idx, user_tensor, user_hist_size,k)
         return total_items
     
-
+    def select_target(self, user_id,targ_test):
+        if self.task=="Top1":
+            ## select Top1 item for Top1 task
+            targ = int(targ_test[user_id][0])
+            targ_indx=0
+        else:
+            ## Random sampling amonth Top10 items for Top10 task
+            targ = np.random.choice(targ_test[user_id])
+            targ_indx=list(targ_test[user_id]).index(targ)
+        return targ, targ_indx
+    
     def predict (self ):
         torch.manual_seed(42)
         np.random.seed(42)
@@ -115,23 +126,33 @@ class PopularityExplainer:
         # number of users for evaluations
 
         test_array=dict_data['test_array']
-        num_of_rand_users = test_array.shape[0] 
+        #num_of_rand_users = test_array.shape[0] 
+        num_of_rand_users = 10
+
         random_rows = np.random.choice(test_array.shape[0], num_of_rand_users, replace=False)
+        #random_sampled_array = test_array[random_rows]
         random_sampled_array = test_array[random_rows]
+
         total_pert=[]  ## size of perturbations
-        print(f'======================== Popularity Explainer run for {self.data_name} and {self.recommender_name}========================')
+        print(f'======================== Popularity Explainer ({self.task} Task) run for {self.data_name} and {self.recommender_name}========================')
 
         for j in range(num_of_rand_users):
             user_id = random_sampled_array[j][-1]
             user_tensor = torch.Tensor(random_sampled_array[j][:-1]).to(self.device)
             user_hist_size=int(torch.sum(user_tensor))
-            targ_itm = np.random.choice(targ_test[user_id])
-            targ_idx=list(targ_test[user_id]).index(targ_itm)
+            ## For Top10 Evalaution
+            targ_itm,targ_idx= self.select_target(user_id,targ_test)
+
+            
 
             targ_vector = dict_data['items_array'][targ_itm]
             total_items= self.calculate_explanation(user_tensor, targ_itm, targ_idx, user_hist_size, k=10)
             if total_items is not None:
                 total_pert.append(total_items)
+            
 
-        print(f'MPNR for Popularity Explainer on {self.data_name} and {self.recommender_name} is ', np.mean(total_pert))
-        print(f'Coverage for Popularity Explainer on {self.data_name} and {self.recommender_name} is ', len(total_pert)*100/num_of_rand_users)
+        print(f'MPNR for Popularity Explainer ({self.task} Task) on {self.data_name} and {self.recommender_name} is ', np.mean(total_pert))
+        print(f'Coverage for Popularity Explainer ({self.task} Task) on {self.data_name} and {self.recommender_name} is ', len(total_pert)*100/num_of_rand_users)
+        
+        with open(Path(Path(os.getcwd(),'scripts'),f'checkpoints/Records_POP_{self.task}_{self.data_name}_{self.recommender_name}.pkl'), 'wb') as f:
+                pickle.dump(total_pert, f)
